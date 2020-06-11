e964dfa091d2e0018ac4b72168409c12a9015900
Bice <59291964+certifiedbice@users.noreply.github.com>
2020-06-11 15:22:38 -0600

    first commit, new repo, project completed

---
+++ README.md
@@ -0,0 +1,34 @@
+## Boilerplate app structure for Quiz App project
+
+#### Created By: 
+#### Taken Over By: Bice: certifiedbice@gmail.com - 06 / 07 / 2020
+
+### Git Repo:
+##### https://github.com/thinkful-ei-panda/Bice-Quiz-App
+
+### Git Page:
+##### https://thinkful-ei-panda.github.io/Bice-Quiz-App/
+
+### Gist:
+##### https://gist.github.com/certifiedbice/d1871831e18d738b92ce96fc7484bd86
+
+### Technical Map:
+#### https://gitmind.com/app/doc/7db354824
+#### https://thinkful-ei-panda.github.io/Bice-Quiz-App/quiz-app-map.jpg
+#### https://thinkful-ei-panda.github.io/Bice-Quiz-App/quiz-app-map.pdf
+
+### Napkin Plan - Quiz App
+
+- Begin screen.
+
+- Form submit, starts the quiz - Question screen.
+
+- Form submit, check answer - Answer screen.
+
+- Form submit, render next quesion - Question screen.
+
+- This process will continue until all questions have been answered.
+
+- Finally, upon submission of last question's answer - Quiz completion screen.
+
+- Form submit, restart quiz - Question screen.
---
+++ app.js
@@ -0,0 +1,347 @@
+const store = {
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'What is broccoli?',
+      answers: ['An alien watchdog', 'A miniature tree', 'A form of torture', 'All of the above'],
+      correctAnswer: 'All of the above',
+    },
+    {
+      question: 'What is the current year?',
+      answers: ['1970', '2015', '2020', '2005'],
+      correctAnswer: '2020',
+    },
+    {
+      question: 'What is a dolpin?',
+      answers: ['Person', 'Place', 'Thing', 'Animal'],
+      correctAnswer: 'Animal',
+    },
+    {
+      question: 'What is your level of pain?',
+      answers: ['No pain', 'Discomfort', 'Intense', 'Unbearable'],
+      correctAnswer: 'Unbearable',
+    },
+    {
+      question: 'What is Winnie the Pooh?',
+      answers: ['A cartoon', 'A stuffed bear', 'A living creature', 'A severe delusion'],
+      correctAnswer: 'A severe delusion',
+    },
+    {
+      question: 'What is Javascript?',
+      answers: ['A hip new brand of coffee', 'A type of language', 'A writers tool', 'A tool actors use to memorize lines'],
+      correctAnswer: 'A type of language',
+    },
+    {
+      question: 'What is the hero\'s name in the Karate kid?',
+      answers: ['Daniel Laruso', 'Johnny Lawrence', 'Tim Burton', 'Luke Skywalker'],
+      correctAnswer: 'Johnny Lawrence',
+    },
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0,
+};
+
+const isAnswered = () => {
+// check for undefined answers and reset the appropriate variables if found.
+
+	//console.log ( 'isAnswered started' );
+
+	if ( store.selectedAnswer === undefined ) {
+
+		//console.log ( 'isAnswered completed' );
+	
+		return false;
+
+	}
+
+	else {
+
+		//console.log ( 'isAnswered completed' );
+
+		return true;
+
+	}
+
+};
+
+const render = ( html ) => {
+//renders the content to the page.
+	
+	//console.log ( 'render started' );
+	
+	$ ( 'main' ).html ( html );
+	
+	//console.log ( 'render completed' );
+	
+};
+
+const generateHTML = () => {
+// Function builds state based html for injection.
+
+	//console.log ( 'generateHTML started' );
+	
+	// Grab the relevant question data for use.
+	if ( store.quizStarted === 0 ) {
+		currentQuestion = store.questions [ store.questionNumber ].question;
+	
+		currentAnswers = store.questions [ store.questionNumber ].answers;
+
+		currentCorrectAnswer = store.questions [ store.questionNumber ].correctAnswer;
+	}
+
+	// Begin screen html.
+	if ( store.quizStarted === false ) {
+		
+		return `
+			<div class="wrapper">
+				<div id="quiz-container">
+					<form id="quiz-app-form" action="http://someform.php">
+						<legend id="quiz-app-form-legend">Click the button below if you dare...</legend>
+						<button type="submit" id="quiz-app-form-submit-button">Begin</button>
+					</form>
+				</div>
+			</div>
+		`;
+
+	}
+
+	// Question screen html.
+	if ( store.quizStarted === 0 ) {
+		
+		let liString = '';
+
+		// Iteration here to build the <li>s for insertion into the output HTML.
+		currentAnswers.forEach ( ( item, index ) => {
+
+			// I realize that there is a better way to implement the button click functionality
+			// through declaring an event handler rather than the onclick used here. This is an 
+			// improvement I would like to see made, but I struggled with the other implementation.
+			// - Bice
+			liString += `<li><input class="quiz-app-form-button" id="form-button-${ index }" type="button" value="${ item }"></input></li>`;
+			
+		});
+
+		return `
+			<div class="wrapper">
+				<div id="quiz-container">
+					<form id="quiz-app-form" action="http://someform.php">
+						<legend id="quiz-app-form-legend">${ currentQuestion }</legend>
+							<ul id="quiz-app-form-ul"> 
+								${ liString }
+							</ul>
+							<button type="submit" id="quiz-app-form-submit-button">Next Question</button>
+					</form>
+					<div>Total questions: ${ store.questions.length }</div>
+					<div>Unanswered questions: ${ store.questions.length - store.questionNumber }</div>
+					<div>Incorrect questions: ${ store.score [ 0 ] }</div>
+					<div>Correct questions: ${ store.score [ 1 ] }</div>
+				</div>
+			</div>
+		`;
+
+	}
+
+	// Answer screen html.
+	else if ( store.quizStarted === 1 ) {
+
+		let answerMsg = '';
+		
+		if ( store.selectedAnswer !== currentCorrectAnswer ) answerMsg = 'Incorrect!';
+
+		else answerMsg = 'Correct!';
+		
+		return `
+			<div class="wrapper">
+				<div id="quiz-container">
+					<form id="quiz-app-form" action="http://someform.php">
+						<legend id="quiz-app-form-legend"><h2>${ answerMsg }</h2></legend>
+						<p>${ currentQuestion }<p>
+						<p>You chose: ${ store.selectedAnswer }</p>
+						<p>The correct answer was: ${ currentCorrectAnswer }</p>
+						<button type="submit" id="quiz-app-form-submit-button">Next question</button>
+					</form>
+				</div>
+			</div>
+		`;
+
+	}
+
+	// Quiz completed.
+	else if ( store.quizStarted === 2 ) {
+		
+		return `
+			<div class="wrapper">
+				<div id="quiz-container">
+					<form id="quiz-app-form" action="http://someform.php">
+						<legend id="quiz-app-form-legend"><h2>Quiz Complete!</h2></legend>
+						<p>Congratulations, you're smart!<p>
+						<p>You answered ${ store.score [ 1 ] } of ${ store.questions.length } questions correctly.</p>
+						<button type="submit" id="quiz-app-form-submit-button">Take the quiz again?</button>
+					</form>
+				</div>
+			</div>
+		`;
+
+	}
+	
+	//console.log ( 'generateHTML completed' );
+
+};
+
+const updateState = () => {
+// Function is responsible for updating the state of quiz.
+
+	//console.log ( 'updateState started' );
+
+	// Begin screen.
+	if ( store.quizStarted === false ) store.quizStarted = 0;
+	
+	// Question screen.
+	else if ( store.quizStarted === 0 ) store.quizStarted = 1;
+	
+	// Answer screen, not the end of the quiz rollback state to continue questions.
+	else if ( store.quizStarted === 1 && store.questionNumber < store.questions.length ) store.quizStarted = 0;
+	
+	// Quiz Complete screen, end of quiz, update state for final screen.
+	else if ( store.quizStarted === 1 && store.questionNumber === store.questions.length ) store.quizStarted = 2;
+
+	//console.log ( 'updateState completed' );
+
+};
+
+const updateScore = () => {
+// Increments store.score if an incorrect answer is submitted.
+
+	//console.log ( 'updateScore started' );
+
+	// Increment incorrect answers
+	if ( store.selectedAnswer !== store.questions [ store.questionNumber ].correctAnswer ) store.score [ 0 ]++;
+
+	// Increment correct answers
+	else store.score [ 1 ]++;
+
+	//console.log ( 'updateScore completed' );
+
+};
+
+const updateQuestion = () => {
+// Increments store.questionNumber.
+		
+	//console.log ( 'updateQuestion started' );
+		
+	store.questionNumber++;
+
+	//console.log ( 'updateQuestion completed' );
+	
+};
+
+const clearLastAnswer = () => {
+// Clear last answer.
+
+	//console.log ( 'clearLastAnswer started' );
+
+	if ( store.quizStarted === 1 ) store.selectedAnswer = undefined;
+
+	//console.log ( 'clearLastAnswer completed' );
+
+};
+
+const formSubmit = () => {
+// Form submit event.
+
+	$ ( 'main' ).on ( 'submit', '#quiz-app-form', e => {
+		
+		//console.log ( 'formSubmitHandler started' );
+
+		e.preventDefault ();
+
+		if ( store.quizStarted === 0 && isAnswered () !== false ){
+
+			updateScore ();
+
+			updateQuestion ();
+
+			updateState ();
+
+		}
+
+		else if ( store.quizStarted === 1 ) {
+
+			clearLastAnswer ();
+
+			updateState ();
+
+		}
+
+		render ( generateHTML );
+
+		//console.log ( 'formSubmitHandler completed' );
+
+	});
+};
+
+const highlightAnswer = () => {
+// Shows visual focus, records selection.
+
+	$ ( 'main' ).on ( 'click', '.quiz-app-form-button', e => {
+		
+		//console.log ( 'highlightAnswer started' );
+			
+		// Reset any previous selection highlight.
+	
+		$ ( '.quiz-app-form-button' ).css ( 'background-color', '#000' );
+	
+		$ ( '.quiz-app-form-button' ).css ( 'color', '#fff' );
+	
+		// Highlight the selected answer.
+	
+		$ ( `#${ e.currentTarget.id }` ).css ( 'background-color', '#fff' );
+	
+		$ ( `#${ e.currentTarget.id }` ).css ( 'color', '#000' );
+	
+		// Store the answer for use.
+	
+		store.selectedAnswer = $ ( `#${ e.currentTarget.id }` ).val ();
+		
+		//console.log ( 'highlightAnswer completed' );
+	
+	});
+
+};
+
+const bindEventHandlers = () => {
+// Bind event handlers.
+
+	//console.log ( 'bindEventHandlers started' );
+
+	// Form submit handler.
+	formSubmit ();
+
+	// Answer click handler.
+	highlightAnswer ();
+
+	//console.log ( 'bindEventHandlers completed' );
+
+};
+
+const init = () => {
+// Initializes by rendering the begin screen, updating state and binding event handlers.
+
+	//console.log ( 'init started' );
+
+	// Set the score property to an array to contain the sums of both pos/neg answers.
+	store.score = [ 0, 0 ];
+
+	render ( generateHTML );
+
+	updateState ();
+
+	bindEventHandlers ();
+
+	//console.log ( 'init completed' );
+
+};
+
+// On page load, call init ().
+$( init );
